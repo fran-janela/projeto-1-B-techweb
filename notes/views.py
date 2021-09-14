a0cc7ae7a2268c80b7588fcdddc37cc7ab969a93
fran-janela <francisco.pinheiro.janela@gmail.com>
2021-09-14 09:22:27 -0300

    projeto-1-A convertido para django

--- notes/views.py
+++ notes/views.py
@@ -4,10 +4,16 @@ from .models import Note
 
 def index(request):
     if request.method == 'POST':
-        note = Note()
-        note.title = request.POST.get('titulo')
-        note.content = request.POST.get('detalhes')
-        note.save()
+        if request.POST.get('method') == 'DELETE':
+            note_delete = Note.objects.get(pk=request.POST.get('id'))
+            note_delete.delete()
+        if request.POST.get('method') == 'UPDATE':
+            Note.objects.filter(id=request.POST.get('id')).update(title=request.POST.get('title'), content=request.POST.get('content'))
+        if request.POST.get('method') == 'POST':
+            note = Note()
+            note.title = request.POST.get('titulo')
+            note.content = request.POST.get('detalhes')
+            note.save()
         return redirect('index')
     else:
         all_notes = Note.objects.all()
