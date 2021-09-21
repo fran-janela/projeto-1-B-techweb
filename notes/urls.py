0f923e9212e476ccda61cf3f4b2fd48c5af75193
fran-janela <francisco.pinheiro.janela@gmail.com>
2021-09-21 10:38:30 -0300

    add tag nas notas

--- notes/urls.py
+++ notes/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('delete/<int:pk>', views.deleteView, name='delete'),
-    path('update/<int:pk>', views.updateViews, name='update'),
+    path('', views.indexNote, name='indexNote'),
+    path('delete/<int:pk>', views.deleteNoteView, name='delete'),
+    path('update/<int:pk>', views.updateNoteViews, name='update'),
 ]
