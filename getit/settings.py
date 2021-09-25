b70b34b76dcce62934bd5dc2e7f468d8397c5d31
fran-janela <francisco.pinheiro.janela@gmail.com>
2021-09-25 18:44:14 -0300

    aplicando modificacoes pro heroku

--- getit/settings.py
+++ getit/settings.py
@@ -25,9 +25,9 @@ SECRET_KEY = 'django-insecure-8sazud#scv2b7&oo+umj5(i_*!m75i!bkr3+!=w*3_u)2z6_^3
 # HEROKU APP: morning-wave-79409
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['morning-wave-79409.herokuapp.com', '0.0.0.0', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -126,6 +127,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
